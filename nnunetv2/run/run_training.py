c7c05ecc02cfdd60f44feadecf8ff26a5431e3da
elebot <elebot@users.noreply.github.com>
2024-07-22 17:10:12 +0000

    add entrypoint for dataset unpacking

--- nnunetv2/run/run_training.py
+++ nnunetv2/run/run_training.py
@@ -12,6 +12,7 @@ from nnunetv2.run.load_pretrained_weights import load_pretrained_weights
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.dataset_name_id_conversion import maybe_convert_to_dataset_name
 from nnunetv2.utilities.find_class_by_name import recursive_find_python_class
+from nnunetv2.training.dataloading.utils import get_case_identifiers, unpack_dataset # add to allow running of unpack dataset;
 from torch.backends import cudnn
 
 
@@ -133,6 +134,40 @@ def run_ddp(rank, dataset_name_or_id, configuration, fold, tr, p, use_compressed
         nnunet_trainer.load_checkpoint(join(nnunet_trainer.output_folder, 'checkpoint_best.pth'))
     nnunet_trainer.perform_actual_validation(npz)
     cleanup_ddp()
+
+
+def run_unpacking(dataset_name_or_id: Union[str, int],
+                  configuration: str, fold: Union[int, str],
+                  trainer_class_name: str = 'nnUNetTrainer',
+                  plans_identifier: str = 'nnUNetPlans',
+                  pretrained_weights: Optional[str] = None,
+                  num_gpus: int = 1,
+                  use_compressed_data: bool = False,
+                  export_validation_probabilities: bool = False,
+                  continue_training: bool = False,
+                  only_run_validation: bool = False,
+                  disable_checkpointing: bool = False,
+                  val_with_best: bool = False,
+                  device: torch.device = torch.device('cuda')):
+
+    if isinstance(fold, str):
+        if fold != 'all':
+            try:
+                fold = int(fold)
+            except ValueError as e:
+                print(f'Unable to convert given value for fold to int: {fold}. fold must bei either "all" or an integer!')
+                raise e
+
+    if val_with_best:
+        assert not disable_checkpointing, '--val_best is not compatible with --disable_checkpointing'
+
+    nnunet_trainer = get_trainer_from_args(dataset_name_or_id, configuration, fold, trainer_class_name,
+                                               plans_identifier, use_compressed_data, device=device)
+    # apply the unpacking to the proposed trainer;
+    unpack_dataset(nnUNet_trainer.preprocessed_dataset_folder, unpack_segmentation=True, overwrite_existing=False,
+                           num_processes=max(1, round(get_allowed_n_proc_DA() // 2)))
+
+
 
 
 def run_training(dataset_name_or_id: Union[str, int],
@@ -268,6 +303,68 @@ def run_training_entry():
     run_training(args.dataset_name_or_id, args.configuration, args.fold, args.tr, args.p, args.pretrained_weights,
                  args.num_gpus, args.use_compressed, args.npz, args.c, args.val, args.disable_checkpointing, args.val_best,
                  device=device)
+
+def run_unpacking_entry():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_name_or_id', type=str,
+                        help="Dataset name or ID to train with")
+    parser.add_argument('configuration', type=str,
+                        help="Configuration that should be trained")
+    parser.add_argument('fold', type=str,
+                        help='Fold of the 5-fold cross-validation. Should be an int between 0 and 4.')
+    parser.add_argument('-tr', type=str, required=False, default='nnUNetTrainer',
+                        help='[OPTIONAL] Use this flag to specify a custom trainer. Default: nnUNetTrainer')
+    parser.add_argument('-p', type=str, required=False, default='nnUNetPlans',
+                        help='[OPTIONAL] Use this flag to specify a custom plans identifier. Default: nnUNetPlans')
+    parser.add_argument('-pretrained_weights', type=str, required=False, default=None,
+                        help='[OPTIONAL] path to nnU-Net checkpoint file to be used as pretrained model. Will only '
+                             'be used when actually training. Beta. Use with caution.')
+    parser.add_argument('-num_gpus', type=int, default=1, required=False,
+                        help='Specify the number of GPUs to use for training')
+    parser.add_argument("--use_compressed", default=False, action="store_true", required=False,
+                        help="[OPTIONAL] If you set this flag the training cases will not be decompressed. Reading compressed "
+                             "data is much more CPU and (potentially) RAM intensive and should only be used if you "
+                             "know what you are doing")
+    parser.add_argument('--npz', action='store_true', required=False,
+                        help='[OPTIONAL] Save softmax predictions from final validation as npz files (in addition to predicted '
+                             'segmentations). Needed for finding the best ensemble.')
+    parser.add_argument('--c', action='store_true', required=False,
+                        help='[OPTIONAL] Continue training from latest checkpoint')
+    parser.add_argument('--val', action='store_true', required=False,
+                        help='[OPTIONAL] Set this flag to only run the validation. Requires training to have finished.')
+    parser.add_argument('--val_best', action='store_true', required=False,
+                        help='[OPTIONAL] If set, the validation will be performed with the checkpoint_best instead '
+                             'of checkpoint_final. NOT COMPATIBLE with --disable_checkpointing! '
+                             'WARNING: This will use the same \'validation\' folder as the regular validation '
+                             'with no way of distinguishing the two!')
+    parser.add_argument('--disable_checkpointing', action='store_true', required=False,
+                        help='[OPTIONAL] Set this flag to disable checkpointing. Ideal for testing things out and '
+                             'you dont want to flood your hard drive with checkpoints.')
+    parser.add_argument('-device', type=str, default='cuda', required=False,
+                    help="Use this to set the device the training should run with. Available options are 'cuda' "
+                         "(GPU), 'cpu' (CPU) and 'mps' (Apple M1/M2). Do NOT use this to set which GPU ID! "
+                         "Use CUDA_VISIBLE_DEVICES=X nnUNetv2_train [...] instead!")
+    args = parser.parse_args()
+
+    assert args.device in ['cpu', 'cuda', 'mps'], f'-device must be either cpu, mps or cuda. Other devices are not tested/supported. Got: {args.device}.'
+    if args.device == 'cpu':
+        # let's allow torch to use hella threads
+        import multiprocessing
+        torch.set_num_threads(multiprocessing.cpu_count())
+        device = torch.device('cpu')
+    elif args.device == 'cuda':
+        # multithreading in torch doesn't help nnU-Net if run on GPU
+        torch.set_num_threads(1)
+        torch.set_num_interop_threads(1)
+        device = torch.device('cuda')
+    else:
+        device = torch.device('mps')
+
+     run_unpacking(args.dataset_name_or_id, args.configuration, args.fold, args.tr, args.p, args.pretrained_weights,
+                 args.num_gpus, args.use_compressed, args.npz, args.c, args.val, args.disable_checkpointing, args.val_best,
+                 device=device)
+
 
 
 if __name__ == '__main__':
