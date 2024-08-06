eb328fb5b1656eb4343431ca4b80fc59ad6a6718
phy <arthur.longuefosse@gmail.com>
2024-08-06 14:43:34 +0000

    notebook : added tuto for double mod

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='nnUNet_translation',
     version='0.1',
-    packages=find_packages(include=['nnunetv2', 'nnunetv2.*']),  # Include specific packages
-    install_requires=[
-        # Your other dependencies
-    ],
-    # Add other setup parameters if necessary
+    packages=find_packages(include=['nnunetv2', 'nnunetv2.*']),
+    include_package_data=True,
+    install_requires=[],
 )
